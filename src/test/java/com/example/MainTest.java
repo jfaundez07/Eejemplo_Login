4fd557df13112f79f3b1aaf4fbd7b1e148ab183a
jfaundez07 <j.faundez07@ufromail.cl>
2024-09-11 11:08:03 -0300

    update(unit test): separate unit tests in different methods

--- src/test/java/com/example/MainTest.java
+++ src/test/java/com/example/MainTest.java
@@ -23,43 +23,39 @@ public class MainTest {
     }
 
     @Test
-    void testGetIndex() {
-            
+    void testGetIndex1() {
         // Test for existing user
-        assertEquals(0, Main.getIndex(users, "samuel01"));
-        assertEquals(1, Main.getIndex(users, "nicolas02"));
         assertEquals(2, Main.getIndex(users, "joaquin03"));
-        
+    }
+
+    @Test
+    void testGetIndex2() {
         // Test for non-existing user
         assertEquals(-1, Main.getIndex(users, "nonexistent"));
-        assertEquals(-1, Main.getIndex(users, "1"));
     }
 
     @Test
-    void testValidateUser() {
-            
+    void testValidateUser1() {  
         // Test for existing user
         assertTrue(Main.validateUser(users, "samuel01"));
-        assertTrue(Main.validateUser(users, "nicolas02"));
-        assertTrue(Main.validateUser(users, "joaquin03"));
-
-        // Test for non-existing user
-        assertFalse(Main.validateUser(users, "phantom_helper"));
-        assertFalse(Main.validateUser(users, "1"));
     }
 
     @Test
-    void testValidateCredentials() {
+    void testValidateUser2() {
+        // Test for non-existing user
+        assertFalse(Main.validateUser(users, "phantom_helper"));
+    }
 
+    @Test
+    void testValidateCredentials1() {
         // Test for correct credentials
-        assertTrue(Main.validateCredentials(passwords, 0, "sepulveda_ufrontera"));
         assertTrue(Main.validateCredentials(passwords, 1, "pelizari_ufromail"));
-        assertTrue(Main.validateCredentials(passwords, 2, "faundez_ufromail"));
+    }
 
+    @Test
+    void testValidateCredentials2() {
         // Test for incorrect credentials
         assertFalse(Main.validateCredentials(passwords, 0, "very_safe_password"));
-        assertFalse(Main.validateCredentials(passwords, 1, "wrong_password"));
-        assertFalse(Main.validateCredentials(passwords, 2, "my_beauty_password"));
     }
 
 }
